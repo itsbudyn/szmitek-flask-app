2cf41c3f288f6e7831dbb241b02a124cb6f4acab
itsbudyn <jakub.kedziora@gmail.com>
2020-07-17 15:13:12 +0200

    Handling 404 and 500 errors

--- flask_app.py
+++ flask_app.py
@@ -56,6 +56,13 @@ def generate():
 def about():
     return render_template("about.html", wyrazy=count1+count2, wyr1=count1, wyr2=count2)
     
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=False)
